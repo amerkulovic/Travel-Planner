38396f48bd2eea132d640e0277da5dad65ac81c9
Amer Kulovic <amer.kulovic@yahoo.com>
2023-01-26 20:56:00 -0600

    added the foreign keys to the index.js

--- models/index.js
+++ models/index.js
@@ -4,10 +4,12 @@ const Trip = require("./Trip");
 
 Traveler.belongsToMany(Location, {
   through: Trip,
+  foreignKey: "traveler_id",
 });
 
 Location.belongsToMany(Traveler, {
   through: Trip,
+  foreignKey:"location_id"
 });
 
 module.exports = { Traveler, Location, Trip };
