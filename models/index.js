d549ef09beb7481a0482c6977eae88176731252e
Amer Kulovic <amer.kulovic@yahoo.com>
2023-01-26 20:54:35 -0600

    filled in the index.js

--- models/index.js
+++ models/index.js
@@ -2,4 +2,12 @@ const Traveler = require("./Traveler");
 const Location = require("./Location");
 const Trip = require("./Trip");
 
+Traveler.belongsToMany(Location, {
+  through: Trip,
+});
+
+Location.belongsToMany(Traveler, {
+  through: Trip,
+});
+
 module.exports = { Traveler, Location, Trip };
