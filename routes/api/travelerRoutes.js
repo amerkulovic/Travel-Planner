c848c09c7ae7d3ba70fd0040bf8e9cfde0b7524b
Amer Kulovic <amer.kulovic@yahoo.com>
2023-01-27 11:49:40 -0600

    added a route to find specific travelers

--- routes/api/travelerRoutes.js
+++ routes/api/travelerRoutes.js
@@ -14,4 +14,21 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/:id", async (req, res) => {
+  try {
+    const travelerData = await Traveler.findByPk(req.params.id, {
+      include: [{ model: Location }],
+    });
+
+    if (!travelerData) {
+      res.status(404).json({ message: "No traveler found with that id!" });
+      return;
+    }
+
+    res.status(200).json(travelerData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
