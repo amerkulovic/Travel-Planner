2eeb2637bba6a14491cc33ce632a4e8903ac9dd5
Amer Kulovic <amer.kulovic@yahoo.com>
2023-01-27 13:16:30 -0600

    added routes to locations

--- routes/api/locationRoutes.js
+++ routes/api/locationRoutes.js
@@ -14,4 +14,38 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/:id", async (req, res) => {
+  try {
+    const locationData = await Location.findByPk(req.params.id, {
+      include: [{ model: Traveler }],
+    });
+
+    if (!locationData) {
+      res.status(404).json({ message: "No location found with that id!" });
+      return;
+    }
+
+    res.status(200).json(locationData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const locationData = await Location.create({
+      location_name: req.body.location_name,
+    });
+    res.status(200).json(locationData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const locationData = await Location.destroy({ where: { id: req.params.id } });
+
+  res.status(200).json(locationData);
+});
+
 module.exports = router;
